65a1048ea17534a255cb8203cfdb5eea8536bb8b
BMWGengiuRobert <robert.gengiu@bmwtechworks.ro>
2026-02-25 16:05:07 +0200

    network table completed, set up the rest of the components

--- frontend/src/app/shared/components/header/header.component.ts
+++ frontend/src/app/shared/components/header/header.component.ts
@@ -2,13 +2,23 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersService } from '../../services/users.service';
 import { AvatarComponent } from '../avatar/avatar.component';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { Router, RouterLink } from '@angular/router';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule, AvatarComponent],
+  imports: [CommonModule, AvatarComponent, MatMenuModule,MatButtonModule,MatIcon,RouterLink, MatTableModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
   private readonly service = inject(UsersService);
+  private readonly router = inject(Router)
+
+  goToSettings(){
+    this.router.navigate(['/settings'])
+  }
 }
